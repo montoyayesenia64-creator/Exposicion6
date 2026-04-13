978ff9ab48c448afc33c029fa2b1de78928cd9cd
montoyayesenia64-creator <montoyayesenia64@gmail.com>
2026-04-13 15:35:22 -0500

    Update Actividad.java
    Actualización dato

--- Actividad.java
+++ Actividad.java
@@ -5,13 +5,13 @@
 import javax.swing.*;
 /**
  *
- * @author INGRESE SU NOMBRE
+ * @author Yesenia Montoya
  */
 public class Actividad {
     public static void main(String args[])
     {
-        String nombre = //COMPLETAR, HACER QUE EL USUARIO INGRESE SU NOMBRE
-        int edad = //COMPLETAR, HACER QUE EL USUARIO INGRESE SU EDAD
+     String nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad");
         String texto = "Hola, mi nombre es " + nombre + ", tengo " + edad + " años, y esta es la actividad de la exposición 6.";
         JOptionPane.showMessageDialog(null, texto);
     }
